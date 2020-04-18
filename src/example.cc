eacc46ab4b6fb42d2fe66a370da712e8e14ec295
nikilr2 <nikilr2@illinois.edu>
2020-04-17 20:18:18 -0500

    Initial commit

--- src/example.cc
+++ src/example.cc
@@ -1,7 +1,14 @@
 // Copyright (c) 2020 [Your Name]. All rights reserved.
 
 #include <mylibrary/example.h>
+#include <cinder/App/App.h>
 
-namespace mylibrary {
+namespace tetris {
 
+class Block {
+  // every block may be represented as a 4 by 4 array,
+  // a rendering function
+  // a getter
+  // color
+};
 }  // namespace mylibrary
